d3a83190e59cf7053a9ae80ef43ab746b1da2f43
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2022-05-31 00:04:20 +0530

    Comments added

--- exampleFiles/script3.sh
+++ exampleFiles/script3.sh
@@ -2,18 +2,20 @@
 
 dir=./$1
 dir_final=./formatted_students.txt
-sed -i '/^/d' $dir_final
+#now delete all previous contents of the file using sed
+sed -i '/^/d' $dir_final 
+#count maintains the count of the line which has been read
 count=1
 while read p; do
 	count=$((count+1))
-	#echo "$count"
+	#increment count by one since you have read the first line 
 	if [ `expr $count % 2` == 0 ]; then
 		person_count=$((count/2))
 		echo -e -n "$person_count;" >> $dir_final
-		#echo "have the string $p"
+		#p now contains the name of a person
 		echo -e -n "$p;" >> $dir_final
 		read p
-		#echo "read the string $p"
+		#p now contains the roll number of a person
 		count=$((count+1))
 		echo -e -n "$p;" >> $dir_final
 		echo -e -n "$p@iith.ac.in\n" | tr '[:upper:]' '[:lower:]' >> $dir_final
